a8aa97e39ce46e0a08dffb6e7580e41d211dd0a2
zhangkf <1399763542@qq.com>
2019-02-23 07:37:10 +0800

    Cache Test

--- platforms/browser/www/js/index.js
+++ platforms/browser/www/js/index.js
@@ -1,4 +1,16 @@
 
+Date.prototype.format = function()
+{
+    var dt = this;
+    var y = dt.getFullYear();
+    var m = dt.getMonth();
+    var d = dt.getDate();
+    var h = dt.getHours();
+    var m = dt.getMinutes();
+    var s = dt.getSeconds();
+    return y + '-' + m + '-' + d + " " + h + ":" + m + ":" + s;
+}
+
 var app = {
     // Application Constructor
     initialize: function () {
@@ -41,12 +53,40 @@ var app = {
         var y = dt.getFullYear();
         var m = dt.getMonth();
         var d = dt.getDate();
-        lab.innerText = "欢迎，您..." + y + '-' + m + '-' + d;
+        lab.innerText = "当前时间：" + new Date().format();
 
         //获取设置信息
         document.getElementById("labDevName").innerText = `设备名称：${device.model}`;
         document.getElementById("labDevSystem").innerText = `操作系统：${device.platform}`;
         document.getElementById("labDevId").innerText = `设备编号：${device.uuid}`;
+
+        document.getElementById("divNavigator").innerHTML = `代码名:${navigator.appCodeName} <br /> 
+        名称：${navigator.appName} <br />
+        操作系统:${navigator.platform} <br />
+        `;
+
+        if (localStorage.loginTime)
+        {
+            document.getElementById("labLastLogin").innerText = `上次登陆时间：${localStorage.loginTime}`;
+        }
+        else
+        {
+            document.getElementById("labLastLogin").innerText = "您是第一次登陆";
+        }
+
+        localStorage.loginTime = new Date().format();
+
+        window.setInterval(function(){
+            var div = document.getElementById("divOnLineSatus");
+            div.innerHTML = `在线状态:${navigator.onLine}`;
+            if (navigator.onLine) div.style.color = "green";
+            else div.style.color = "red";
+        },1000)
+
+        var btnRefresh = document.getElementById("btnRefresh");
+        window.addEventListener("click",function(){
+            window.location.reload();
+        },false);
     },
 
     receivedEvent: function (id) {
